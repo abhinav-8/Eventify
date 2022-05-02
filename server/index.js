faf8ac68478927cfff0df366e6e2473d00150cfa
Tushar Garg <tshrgarg2010@gmail.com>
2022-05-02 23:39:05 +0530

    Added routes, controllers and models in backend

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//URLS go as localhost:5000/posts
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL =
+  "mongodb+srv://assassin1771:assassin1771@cluster0.kwwcd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set("useFindAndModify", false);
