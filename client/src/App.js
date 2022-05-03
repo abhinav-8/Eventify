8e692a68c6ebd3e9b23b2a653b8e143532897fda
Tushar Garg <tshrgarg2010@gmail.com>
2022-05-03 23:26:31 +0530

    Setup Redux to connect to backend and made form for user entry

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import memories from "./images/memories.png";
 import useStyles from "./styles.js";
@@ -6,8 +6,18 @@ import useStyles from "./styles.js";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 
+import { getPosts } from "./actions/posts";
+
+import { useDispatch } from "react-redux";
+
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
