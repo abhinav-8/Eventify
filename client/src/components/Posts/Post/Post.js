1465326ffbaf0d6b36eb1d1ac113e694410767cd
Tushar Garg <tshrgarg2010@gmail.com>
2022-06-15 19:06:29 +0530

    Code Refactor

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -78,23 +78,9 @@ const Post = ({ post, setCurrentId }) => {
           <ThumbUpAltIcon fontSize="small" /> &nbsp; ATTENDING EVENT? (
           {post.likeCount})
         </Button>
-        {/* <Button
-          size="small"
-          color="primary"
-          onClick={() => dispatch(deletePost(post._id))}
-        >
-          <DeleteIcon fontSize="small" /> Delete
-        </Button> */}
       </CardActions>
 
       <CardActions className={classes.cardActions}>
-        {/* <Button
-          size="small"
-          color="primary"
-          onClick={() => dispatch(likePost(post._id))}
-        >
-          <ThumbUpAltIcon fontSize="small" /> LIKE {post.likeCount}{" "}
-        </Button> */}
         <Button
           size="small"
           color="primary"
