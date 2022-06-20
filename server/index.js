f36900e4b78bb671c490f5f13df06a3364f7d678
Tushar Garg <tshrgarg2010@gmail.com>
2022-06-20 18:25:37 +0530

    Added SS and MONGODB URI

--- server/index.js
+++ server/index.js
@@ -25,10 +25,13 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.kwwcd.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
